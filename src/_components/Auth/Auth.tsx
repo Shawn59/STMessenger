f693b1feed379d1dd346b4175b5928c4151ad0b8
shawn59 <xxx_respekt.ru@mail.ru>
2026-02-18 19:51:39 +0500

    main - Дополнил форму входа

--- src/_components/Auth/Auth.tsx
+++ src/_components/Auth/Auth.tsx
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import { actionSetUser } from '../../store/userSlice/user.slice';
 import type { IUserModel } from '../../types/user';
 import uuid from 'react-uuid';
+import { MaskInput } from '../../_atoms/MaskInput/MaskInput';
 
 export const Auth = () => {
   const {
@@ -51,6 +52,7 @@ export const Auth = () => {
         error={!!errors.firstName}
         helperText={errors.firstName?.message}
         autoComplete={'off'}
+        variant={'standard'}
       />
 
       <TextField
@@ -66,6 +68,7 @@ export const Auth = () => {
         error={!!errors.lastName}
         helperText={errors.lastName?.message}
         autoComplete={'off'}
+        variant={'standard'}
       />
 
       <TextField
@@ -81,35 +84,64 @@ export const Auth = () => {
         error={!!errors.profession}
         helperText={errors.profession?.message}
         autoComplete={'off'}
+        variant={'standard'}
       />
 
       <TextField
         {...register('email', {
           maxLength: { value: 20, message: 'Максимум 20 символов' },
           pattern: {
-            value: /^\S+$/,
-            message: 'Некорректное значение',
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: 'Некорректный email',
           },
         })}
         label={'Email'}
         error={!!errors.email}
         helperText={errors.email?.message}
         autoComplete={'off'}
+        variant={'standard'}
       />
 
-      <TextField
-        {...register('phone', {
-          maxLength: { value: 20, message: 'Максимум 20 символов' },
-          pattern: {
-            value: /^\S+$/,
-            message: 'Некорректное значение',
-          },
-        })}
+      <MaskInput
         label={'Телефон'}
+        mask="# (000) 000-00-00"
+        definitions={{
+          '#': /[1-9]/,
+        }}
         error={!!errors.phone}
         helperText={errors.phone?.message}
-        autoComplete={'off'}
+        inputModeIsNumeric={'numeric'}
+        {...register('phone', {
+          minLength: { value: 17, message: 'некорректный номер' },
+        })}
       />
+
+      {/* <TextField
+        label="Телефон"
+        error={!!errors.phone}
+        helperText={errors.phone?.message}
+        fullWidth
+        margin="normal"
+        InputProps={{
+          inputComponent: PhoneMask,
+        }}
+      />*/}
+
+      {/*<InputMask mask="(0)999 999 99 99" value={'+7'} maskChar=" ">
+        <TextField
+          {...register('phone', {
+            maxLength: { value: 20, message: 'Максимум 20 символов' },
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'Некорректный email',
+            },
+          })}
+          label={'Телефон'}
+          error={!!errors.phone}
+          helperText={errors.phone?.message}
+          autoComplete={'off'}
+        />
+      </InputMask>*/}
 
       <Button type="submit" variant="contained" color="primary">
         {'Отправить'}
